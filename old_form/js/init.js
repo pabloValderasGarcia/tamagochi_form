11f05296282c849d1f9b7c2a114ae2c45e5c3e47
Pablo Valderas Garcia <pvalgarn@gmail.com>
2022-11-14 19:57:10 +0100

    Initial commit

---
+++ old_form/js/init.js
@@ -0,0 +1,30 @@
+import { createForm, menuInterface } from './script.js';
+
+let form = {
+    "id" : "tamagochiForm",
+    "action" : "something",
+    "elements" : [
+        {
+            "id" : "chk1",
+            "type" : "checkbox"
+        },
+        {
+            "id" : "chk2",
+            "type" : "checkbox"
+        }, {
+            "id" : "chk3",
+            "type" : "checkbox"
+        },
+        {
+            "id" : "range1",
+            "type" : "range"
+        },
+        {
+            "id" : "submit1",
+            "type" : "submit"
+        }
+    ]
+}
+createForm(form);
+
+menuInterface();
